6d1045d2e92a463b6da139c5fceeaaf446e561ea
Christophe Vanlancker <carroarmato0@inuits.eu>
2019-10-08 00:03:40 +0200

    Very basic database query for existing expenses
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -6,13 +6,48 @@
   use Symfony\Component\Routing\Annotation\Route;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+  use App\Entity\Expense;
+  use App\Entity\User;
+
   class ExpenseController extends AbstractController {
 
     /**
      * @Route("/")
      */
     public function index() {
-      return $this->render('expenses/index.html.twig');
+
+      // Create my User object, we assume we are Admin for now
+      $admin = $this->getDoctrine()->getRepository(User::class)->find(1);
+
+      // Get expens repository
+      $expense_repository = $this->getDoctrine()->getRepository(Expense::class);
+      // Fetch user expenses ordered by date
+      $my_expenses = $expense_repository->findBy(
+        [ 'user_id' => $admin->getId() ],
+        [ 'date'    => 'DESC'          ]
+      );
+
+
+
+      /**
+      $entityManager = $this->getDoctrine()->getManager();
+      $admin_user = $this->getDoctrine()
+        ->getRepository(User::class)
+        ->find(1);
+      $expense = new Expense();
+      $expense->setUserId($admin_user);
+      $expense->setSubmitDate( new \DateTime() );
+      $expense->setName('Total');
+      $expense->setDescription('Tankbeurt');
+      $expense->setDate( new \DateTime());
+      $expense->setAmount( 60.45 );
+      $expense->setReimbursed(0);
+      $entityManager->persist($expense);
+      $entityManager->flush();
+      */
+
+      return $this->render('expenses/index.html.twig', array(
+        'expenses' => $my_expenses));
     }
 
   }
