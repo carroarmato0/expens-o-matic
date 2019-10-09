25474a76ad30b150b3b9f9d2cc5fe21a1fc7a59b
Christophe Vanlancker <carroarmato0@inuits.eu>
2019-10-09 23:14:13 +0200

    Remove unused FileType
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- src/Forms/ExpenseFormType.php
+++ src/Forms/ExpenseFormType.php
@@ -6,7 +6,6 @@ use App\Entity\Expense;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
