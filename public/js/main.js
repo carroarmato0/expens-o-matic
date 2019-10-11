71133141b2c5c9ec66014cf66b9ac9bc9ecf2a80
Christophe Vanlancker <carroarmato0@inuits.eu>
2019-10-11 10:40:20 +0200

    Make the JS on-click listener for deleting expenses more targetting for a specific class
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,9 @@ const expenses = document.getElementById('expenses');
 
 if (expenses) {
   expenses.addEventListener('click', e => {
-    if (e.target.className === 'btn btn-primary btn-sm delete-expense') {
+    if ( $(e.target).hasClass('delete-expense')) {
       if (confirm('Are you sure?')) {
         const id = e.target.getAttribute('data-id');
-
-        //fetch("/expense/${id}/delete", {
-        //  method: 'DELETE'
-        //}).then(res => window.location.reload());
 
         fetch("/expense/" + id + "/delete", {
           method: 'DELETE'
