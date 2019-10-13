9525ff018bca5488fa6331edbf68a2f0f270001c
Christophe Vanlancker <carroarmato0@inuits.eu>
2019-10-14 01:00:57 +0200

    Add Account and Admin controllers. Reflect current page in Navbar
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
