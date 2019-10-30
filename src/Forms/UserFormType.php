fdc6b3023a33baf2c4456ab4b5112b580d74bc95
Christophe Vanlancker <carroarmato0@inuits.eu>
2019-10-31 00:29:27 +0100

    Add Roles hierachy and allow setting the role upon user creation
    Signed-off-by: Christophe Vanlancker <carroarmato0@inuits.eu>

--- src/Forms/UserFormType.php
+++ src/Forms/UserFormType.php
@@ -4,10 +4,12 @@
 namespace App\Forms;
 
 use App\Entity\User;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -58,11 +60,39 @@ class UserFormType extends AbstractType
           'autocomplete'  => 'off',
         ],
       ])
+      ->add("roles", ChoiceType::class, [
+          'label'   => 'Role',
+          'label_attr' => [
+            'class'         => 'font-weight-bold',
+          ],
+          'choices' => array(
+            'Admin'     => 'ROLE_ADMIN',
+            'Approver'  => 'ROLE_APPROVER',
+            'User'      => 'ROLE_USER',
+          ),
+          'data' => 'ROLE_USER',
+        ]
+      )
       ->add("submit", SubmitType::class, [
         'attr'       => [
           'class' => 'btn btn-primary'
         ],
       ])
+    ;
+
+    $builder
+      ->get('roles')
+      ->addModelTransformer(new CallbackTransformer(
+        function ($rolesAsArray) {
+          // Transform the Array to a String
+          return $rolesAsArray;
+        },
+
+        function ($rolesAsString) {
+          // Transform the String back to Array
+          return array($rolesAsString);
+        }
+      ))
     ;
 
   }
